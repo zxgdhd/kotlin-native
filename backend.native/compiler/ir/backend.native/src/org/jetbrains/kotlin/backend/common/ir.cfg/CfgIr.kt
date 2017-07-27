99f79052129918f62dd2d39c71e85ff698a6a5fa
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2017-07-27 11:11:14 +0700

    Merge

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/common/ir.cfg/CfgIr.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/common/ir.cfg/CfgIr.kt
@@ -2,24 +2,6 @@ package org.jetbrains.kotlin.backend.common.ir.cfg
 
 //-----------------------------------------------------------------------------//
 
-sealed class Type {
-    abstract val byteSize: Int
-
-    object boolean: Type() { override val byteSize: Int get() = 1 }
-    object byte   : Type() { override val byteSize: Int get() = 2 }
-    object short  : Type() { override val byteSize: Int get() = 2 }
-    object int    : Type() { override val byteSize: Int get() = 4 }
-    object long   : Type() { override val byteSize: Int get() = 8 }
-    object float  : Type() { override val byteSize: Int get() = 4 }
-    object double : Type() { override val byteSize: Int get() = 8 }
-    object char   : Type() { override val byteSize: Int get() = 2 }
-    class ptr<out T>(val value: T) : Type() { override val byteSize: Int get() = 8 }
-
-    override fun toString() = asString()
-}
-
-//-----------------------------------------------------------------------------//
-
 abstract class Operand(val type: Type) {
     val uses = mutableListOf<Instruction>()
     val defs = mutableListOf<Instruction>()
