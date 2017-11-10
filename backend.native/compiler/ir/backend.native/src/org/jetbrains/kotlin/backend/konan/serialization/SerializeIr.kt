736fb6938d26117cca57921bafa3136aa89b34b2
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-11-10 16:41:13 +0300

    IrGetClass serialization/deserialization.

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/SerializeIr.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/SerializeIr.kt
@@ -256,6 +256,12 @@ internal class IrSerializer(val context: Context,
         return proto.build()
     }
 
+    fun serializeGetClass(expression: IrGetClass): KonanIr.IrGetClass {
+        val proto = KonanIr.IrGetClass.newBuilder()
+            .setArgument(serializeExpression(expression.argument))
+        return proto.build()
+    }
+
     fun serializeGetEnumValue(expression: IrGetEnumValue): KonanIr.IrGetEnumValue {
         val proto = KonanIr.IrGetEnumValue.newBuilder()
             .setType(serializeKotlinType(expression.type))
@@ -449,6 +455,7 @@ internal class IrSerializer(val context: Context,
             is IrContinue    -> operationProto.setContinue(serializeContinue(expression))
             is IrDelegatingConstructorCall
                              -> operationProto.setDelegatingConstructorCall(serializeDelegatingConstructorCall(expression))
+            is IrGetClass    -> operationProto.setGetClass(serializeGetClass(expression))
             is IrGetField    -> operationProto.setGetField(serializeGetField(expression))
             is IrGetValue    -> operationProto.setGetValue(serializeGetValue(expression))
             is IrGetEnumValue    
@@ -821,6 +828,11 @@ internal class IrDeserializer(val context: Context,
         return call
     }
 
+    fun deserializeGetClass(proto: KonanIr.IrGetClass, start: Int, end: Int, type: KotlinType): IrGetClass {
+        val argument = deserializeExpression(proto.argument)
+        return IrGetClassImpl(start, end, type, argument)
+    }
+
     fun deserializeGetField(proto: KonanIr.IrGetField, start: Int, end: Int): IrGetField {
         val access = proto.fieldAccess
         val descriptor = deserializeDescriptor(access.getDescriptor()) as PropertyDescriptor
@@ -1036,6 +1048,8 @@ internal class IrDeserializer(val context: Context,
                 -> deserializeDelegatingConstructorCall(proto.delegatingConstructorCall, start, end)
             GET_ENUM_VALUE
                 -> deserializeGetEnumValue(proto.getEnumValue, start, end)
+            GET_CLASS
+                -> deserializeGetClass(proto.getClass, start, end, type)
             GET_FIELD
                 -> deserializeGetField(proto.getField, start, end)
             GET_OBJECT
